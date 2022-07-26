6e5008b3875b85592542fb02050461f3129b2e8b
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-07-26 12:24:46 -0300

    Updates

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,25 @@
 import { userLogin, userGoogle } from "../lib/authentication.js";
 
 export default () => {
-  const container = document.createElement("form");
+  const container = document.createElement("div");
   const templateLoging = `
-  <input class="email" placeholder ="e-mail" type="email" required>
-  <span class="email-error"></span>
-  <input class="password" placeholder="senha" minlength="6" type="password" required>
-  <span class="password-error"></span>
-  <button class="enter" type="submit">Entrar</button>
-  <p>ou</p>
-  <button class="google" type="submit"><span class="googleLogin">Login com o GOOGLE</span></button>
-  <div class="register"><a href="#register">Cadastre-se</a></div>
-  `;
+  <div class= "main-container">
+    <div class= "title">
+      <img src="../images/title.png" alt="leiturama" class= "title-image">
+    </div>
+    <div class= "flex-container">
+      <form>
+        <input class="email" placeholder ="e-mail" type="email" required>
+        <span class="email-error"></span>
+        <input class="password" placeholder="senha" minlength="6" type="password" required>
+        <span class="password-error"></span>
+        <button class="enter" type="submit">Entrar</button>
+        <p class= "login-google">Entre com sua conta Google</p>
+        <button class="google" type="submit"><img class= "google-button" src= "../images/google.png" height ="30" width="30"/></button>
+        <div class="register"><a href="#register">Cadastre-se</a></div>
+      </form>
+    </div>
+  </div>`;
   container.innerHTML = templateLoging;
 
   const email = container.querySelector(".email");
